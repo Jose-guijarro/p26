7e403d6ae89d95857ba99345802d2e2c4b480795
18PROGB0452 <18PROGB0452@TS-LABS-2.labs.cbtis94.local>
2019-11-15 13:20:01 -0600

    Se creo la interfas

---
+++ practica26/src/practica26/Practica26.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practica26;
+
+/* @author 18PROGB0452 */
+public class Practica26 {
+
+  
+    public static void main(String[] args) {
+        // Escribe aui tu codigo.....
+    }
+
+}
